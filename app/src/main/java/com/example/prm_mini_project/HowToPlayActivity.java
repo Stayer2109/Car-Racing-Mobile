a6234564c384c5a495cb8a09d0ea0e2ec9834742
Stayer2109 <nthanhphong2109@gmail.com>
2024-09-29 11:41:50 +0700

    features: add how to play activity

--- app/src/main/java/com/example/prm_mini_project/HowToPlayActivity.java
+++ app/src/main/java/com/example/prm_mini_project/HowToPlayActivity.java
@@ -1,6 +1,8 @@
 package com.example.prm_mini_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HowToPlayActivity extends AppCompatActivity {
+    // Variables
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class HowToPlayActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Mappings
+        backButton = findViewById(R.id.backButton);
+
+        // Set onClickListener For Back Button
+        backButton.setOnClickListener(v -> {
+            finish();
         });
     }
 }
