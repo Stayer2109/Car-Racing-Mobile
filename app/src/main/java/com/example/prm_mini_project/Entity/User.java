f0bdb8f94748dc0d643937d2c8cc43217920c3c1
Stayer2109 <nthanhphong2109@gmail.com>
2024-09-29 11:58:23 +0700

    features: add user entity, add signup activity

---
+++ app/src/main/java/com/example/prm_mini_project/Entity/User.java
@@ -0,0 +1,34 @@
+package com.example.prm_mini_project.Entity;
+
+public class User {
+    private String username;
+    private String password;
+    private double balance;
+
+    public User() {
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- app/src/main/java/com/example/prm_mini_project/SignUpAcvitity.java
+++ app/src/main/java/com/example/prm_mini_project/SignUpAcvitity.java
@@ -1,8 +1,6 @@
 package com.example.prm_mini_project;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    // Variables
-    Button howToPlayButton;
-    Button signUnButton;
+public class SignUpAcvitity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up_acvitity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Mappings
-        howToPlayButton = findViewById(R.id.howToPlayButton);
-        signUnButton = findViewById(R.id.signUpButton);
-
-        //region Set onClickListener For How Buttons
-        howToPlayButton.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, HowToPlayActivity.class));
-        });
-
-        signUnButton.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, SignUpAcvitity.class));
-        });
-        //endregion
-
     }
 }
